a715c8f0004870a4776b277db5dfcb6969b144fe
Ratchanan Srirattanamet <ratchanan@ubports.com>
2019-11-06 23:47:35 +0700

    card: read headphone availability from input device
    Recent Android devices start to provide headphone availability via input
    device instead of h2w switch. This renders droid-extcon useless.
    
    This commit introduce droid-extevdev, a simple code that will read
    headphone availability from the input device, using libevdev as an
    abstraction layer. This means the code now depends on libevdev (but it
    can be made optional later on if needed).
    
    This should make headphone availability works on newer Android devices
    without having to resort to h2w kernel driver.
    
    PATCH: extevdev: make the combo port always available
    
    Ringtone & alarm uses the combo port to play to both the headphone and
    the speaker. But because we distinguish between headphone and headset,
    the port won't be available if we plug a headset with a mic.
    
    Turns out, droid-extcon doesn't do the availability thing on this port,
    so the port is always available. So, we should do the same.
    
    [card] read headphone availability from input device.
    
    Merged from UBports.

--- src/droid/module-droid-card.c
+++ src/droid/module-droid-card.c
@@ -64,6 +64,7 @@
 #include "droid-sink.h"
 #include "droid-source.h"
 #include "droid-extcon.h"
+#include "droid-extevdev.h"
 
 PA_MODULE_AUTHOR("Juho Hämäläinen");
 PA_MODULE_DESCRIPTION("Droid card jb2q");
@@ -162,6 +163,7 @@ struct userdata {
     pa_card_profile *real_profile;
 
     pa_droid_extcon *extcon;
+    pa_droid_extevdev *extevdev;
 
     pa_modargs *modargs;
     pa_card *card;
@@ -852,6 +854,11 @@ int pa__init(pa_module *m) {
     init_profile(u);
     u->extcon = pa_droid_extcon_new(m->core, u->card);
 
+    if (!u->extcon)
+        u->extevdev = pa_droid_extevdev_new(m->core, u->card);
+    else
+        u->extevdev = NULL;
+
     pa_card_put(u->card);
 
     return 0;
@@ -881,6 +888,8 @@ void pa__done(pa_module *m) {
         if (u->card && u->card->sources)
             pa_idxset_remove_all(u->card->sources, (pa_free_cb_t) pa_droid_source_free);
 
+        if (u->extevdev)
+            pa_droid_extevdev_free(u->extevdev);
 
         if (u->card)
             pa_card_free(u->card);
